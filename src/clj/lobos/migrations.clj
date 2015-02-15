ac4de0f48c3faad1d254774196eaf018ea2d3e80
Maikol <miguel.deelias@gmail.com>
2015-02-15 04:33:07 -0300

    changed migrations

--- src/clj/lobos/migrations.clj
+++ src/clj/lobos/migrations.clj
@@ -10,17 +10,20 @@
 (defmigration add-recipes-table
 	(up [] (create
 		(table :recipes
-			(integer :id :primary-key :auto-inc )
-			(varchar :name 100 )
-			(timestamp :created (default (now))))))
+			(integer 		:id :primary-key :auto-inc )
+			(varchar 		:name 100 )
+			(varchar 		:instructions 1000)
+			(varchar 		:avatar_url 100)
+			(timestamp 	:created (default (now))))))
 	(down [] (drop (table :recipes ))))
 
 (defmigration add-ingredients-table
 	(up [] (create
 					(table :ingredients
-						(integer :id :primary-key :auto-inc )
-						(varchar :name 250)
-						(varchar :units 100)
-						(timestamp :created (default (now)))
-						(integer :recipe_id [:refer :recipes :id] :not-null))))
+						(integer 		:id :primary-key :auto-inc )
+						(varchar 		:name 250)
+						(varchar 		:units 100)
+						(double			:quantity)
+						(timestamp 	:created (default (now)))
+						(integer 		:recipe_id [:refer :recipes :id] :not-null))))
 	(down [] (drop (table :ingredients ))))
